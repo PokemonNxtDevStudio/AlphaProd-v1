09837b15144b86656021442f0bf1ceff1bb4db4f
Cody Brill <ncbrill@gmail.com>
2015-04-21 16:30:57 -0700

    Added a very basic InGame menu assets/scenes/UI/InGameMenu.unity

---
+++ Assets/Scripts/UI/InGameMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameMenu : MonoBehaviour 
+{
+    [HideInInspector]
+    public bool menuEnabled = false;
+    public GameObject menuObject;
+
+	void Update () 
+    {
+        if (cInput.GetKeyDown("Menu"))
+        {
+            ToggleMenu();
+        }
+	}
+
+    public void ToggleMenu()
+    {
+        if (menuEnabled) // Turning menu off
+        {
+            menuEnabled = false;
+            menuObject.SetActive(false);
+        }
+        else if (!menuEnabled) // Turning menu on
+        {
+            menuEnabled = true;
+            menuObject.SetActive(true);
+        }
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
