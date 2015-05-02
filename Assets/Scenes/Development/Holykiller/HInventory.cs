c4b381634b95e6dc6833a58d70af58709ed8908d
abraham sanchez <abraham_gto@hotmail.com>
2015-05-02 08:25:10 -0500

    inventory basic ui

---
+++ Assets/Scenes/Development/Holykiller/HInventory.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using NXT;
+using NXT.Inventory;
+
+public class HInventory : MonoBehaviour 
+{
+    [SerializeField]
+    private Sprite[] PokemonIcons;
+
+    [SerializeField]
+    private GameObject InventoryUI;
+
+    [SerializeField]
+    private Inventory _Inv;
+    
+	
+	void Start () 
+    {
+        _Inv = new Inventory();
+	}
+	
+	
+	void Update () 
+    {
+
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            if(InventoryUI != null)
+            {
+                InventoryUI.SetActive(!InventoryUI.activeSelf);
+            }
+        }
+
+	}
+}
