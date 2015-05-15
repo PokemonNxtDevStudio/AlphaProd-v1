a116f69c363ca92f075e5ab6d73665a63d764368
Michael <phanmicheal@gmail.com>
2015-05-15 19:55:17 -0400

    blocked out trainer input handler

--- Assets/Scripts/Controllers/Input/User/TrainerInputHandler.cs
+++ Assets/Scripts/Controllers/Input/User/TrainerInputHandler.cs
@@ -23,6 +23,26 @@ public class TrainerInputHandler : MonoBehaviour {
 			motor.MoveCharacter(SharedConstants.Movement.left);
 		}
 
+		if (Input.GetKeyUp(KeyCode.I)) {
+			//open inventeory
+		}
+		if (Input.GetKeyUp(KeyCode.R)) {
+			//Release pokemon if no pokemon
+			//or
+			//Return pokemon if pokemon is out
+		}
+
+		if (Input.GetKeyUp (KeyCode.Alpha1))
+			//cast skill 1
+		if (Input.GetKeyUp (KeyCode.Alpha2))
+			//cast skill 2
+		if (Input.GetKeyUp (KeyCode.Alpha3))
+			//cast skill 3
+		if (Input.GetKeyUp (KeyCode.Alpha4))
+			//cast skill 4
+		if (Input.GetKeyUp (KeyCode.Alpha5))
+			//cast skill 5
+
 	}
 	void OnCollision(Collider collision){
 		//handlec quicktrigger with shops and such
