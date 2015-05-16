5d67398fb8cd5fa44363fb0c43ca166567aa9dee
Michael <phanmicheal@gmail.com>
2015-05-16 00:23:07 -0400

    merged

--- Assets/Scripts/Controllers/Input/User/TrainerInputHandler.cs
+++ Assets/Scripts/Controllers/Input/User/TrainerInputHandler.cs
@@ -4,13 +4,12 @@ using System.Collections;
 public class TrainerInputHandler : MonoBehaviour {
 	[SerializeField]
 	private MoterInputHandler motor;
-	public delegate void Commands(SharedConstants.Commands command);
-	public Commands commands;
 
 	// Update is called once per frame
 	void Update(){
 		readInputs ();
 	}
+
 	void readInputs(){
 		//handle cases for triggering release/capture/cast/inventory/etc
 		if (motor) {
