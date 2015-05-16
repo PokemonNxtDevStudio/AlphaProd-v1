9ca9f8e74a2cf31594c4fbf03921d0291d06c50b
Michael <phanmicheal@gmail.com>
2015-05-16 15:22:54 -0400

    created global delegates

--- Assets/Scripts/Controllers/Actors/TrainerController.cs
+++ Assets/Scripts/Controllers/Actors/TrainerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TrainerController : MonoBehaviour
 {
     private PokeParty pokeParty; 
+	private int pokeSlot;
     public List<String> pokemon;
 	//public SharedConstants.CharacterType characterType;
 
@@ -19,23 +20,42 @@ public class TrainerController : MonoBehaviour
     }
 
 	void OnEnable(){
-//		TrainerInputHandler.Commands += OnCommand;
+		TrainerInputHandler.COMMANDS += OnCommand;
 	}
 
 	void Update() {
 		
 	}
 
-	private void OnCommand(){
-
+	private void OnCommand(KeyCode __command){
+		switch (__command) {
+			case KeyCode.R:
+				TogglePokemon();
+				break;
+			case KeyCode.Alpha1:
+				UseSkill(0);
+				break;
+			case KeyCode.Alpha2:
+				UseSkill(1);
+				break;
+			case KeyCode.Alpha3:
+				UseSkill(2);
+				break;
+			case KeyCode.Alpha4:
+				UseSkill(3);
+				break;
+			case KeyCode.Alpha5:
+				UseSkill(4);
+				break;
+		}
 	}
 
-    public void ReleasePokemon(int __slot)
+    public void TogglePokemon()
     {
-		Debug.Log("Releasing " + pokemon[__slot]);
+		Debug.Log("Releasing "+pokemon[pokeSlot]);
     }
 	
-	public void UseSkill(int __skillID)
+	public void UseSkill(int __skillSlotID)
 	{
 //		Debug.Log("Using Skill ");
 	}
@@ -44,6 +64,12 @@ public class TrainerController : MonoBehaviour
 	{
 //		Debug.Log("usingItem ");
 	}
+	
+	public void OpenInventory()
+	{
+		//		Debug.Log("usingItem ");
+	}
+
 
     
 
--- Assets/Scripts/Controllers/Input/User/TrainerInputHandler.cs
+++ Assets/Scripts/Controllers/Input/User/TrainerInputHandler.cs
@@ -5,9 +5,18 @@ public class TrainerInputHandler : MonoBehaviour {
 	[SerializeField] private Camera camera;
 	[SerializeField] private float speed;
 	private MotorController motor;
+	private TrainerController trainerController;
+
+	//events to subscribe to
+	public delegate void inputAction(KeyCode keys);
+	public static event inputAction Movement;
+	public static event inputAction COMMANDS;
+	public static event inputAction UI;
+
 	void Start(){
 		speed = speed == 0 ? .2f : speed;
 		motor = GetComponent<MotorController> ();
+		trainerController = GetComponent<TrainerController> ();
 	}
 
 	// Update is called once per frame
@@ -20,51 +29,56 @@ public class TrainerInputHandler : MonoBehaviour {
 		if (motor) {
 			if (Input.GetKey(KeyCode.W)) {
 //				motor.MoveCharacter(SharedConstants.Movement.forward);
-				Vector3 facingAngle = camera.transform.eulerAngles;
-				Vector3 facePos = camera.transform.position;
-				transform.eulerAngles = new Vector3(transform.eulerAngles.x, facingAngle.y, transform.eulerAngles.z);
-				camera.transform.eulerAngles = new Vector3(facingAngle.x, facingAngle.y, facingAngle.z); 
-				camera.transform.position = new Vector3(facePos.x, facePos.y, facePos.z); 
-				motor.Move(Vector3.forward*speed);
+				faceCamera();
+				Movement(KeyCode.W);
+//				motor.Move(Vector3.forward*speed);
 			}
 			if (Input.GetKeyUp(KeyCode.S)) {
-				motor.Move(Vector3.back*speed);
-//				motor.MoveCharacter(SharedConstants.Movement.back);
+				Movement(KeyCode.S);
+//				motor.Move(Vector3.back*speed);
 			}
 			if (Input.GetKeyUp(KeyCode.D)) {
-				motor.Move(Vector3.right*speed);
-//				motor.MoveCharacter(SharedConstants.Movement.right);
+				Movement(KeyCode.D);
+//				motor.Move(Vector3.right*speed);
 			}
 			if (Input.GetKeyUp(KeyCode.A)) {
-				motor.Move(Vector3.left*speed);
-//				motor.MoveCharacter(SharedConstants.Movement.left);
+				Movement(KeyCode.A);
+//				motor.Move(Vector3.left*speed);
 			}
 		}
 
-		if (Input.GetKeyUp(KeyCode.I)) {
-			//open inventeory
-		}
-		if (Input.GetKeyUp(KeyCode.R)) {
+		if (Input.GetKeyUp(KeyCode.I))
+			UI(KeyCode.I);
+		checkTrainerCommands ();
+		checkItemUse ();
+	}
+
+	public void checkItemUse() {
+
+	}
+
+	public void checkTrainerCommands(){
+		if (Input.GetMouseButtonUp(0)) {
+			UI(KeyCode.R);
 			//Release pokemon if no pokemon
 			//or
 			//Return pokemon if pokemon is out
 		}
-
-		if (Input.GetKeyUp (KeyCode.Alpha1)) {
-			//cast skill 1
-		}
-		if (Input.GetKeyUp (KeyCode.Alpha2)){
-			//cast skill 2
-		}
-		if (Input.GetKeyUp (KeyCode.Alpha3)){
-			//cast skill 3
-		}
-		if (Input.GetKeyUp (KeyCode.Alpha4)){
-			//cast skill 4
-		}
-		if (Input.GetKeyUp (KeyCode.Alpha5)){
-			//cast skill 5
-		}
+		//cast skill 1
+		if (Input.GetKeyUp (KeyCode.Alpha1))
+			UI(KeyCode.Alpha1);
+		//cast skill 2
+		if (Input.GetKeyUp (KeyCode.Alpha2))
+			UI(KeyCode.Alpha2);
+		//cast skill 3
+		if (Input.GetKeyUp (KeyCode.Alpha3))
+			UI(KeyCode.Alpha3);
+		//cast skill 4
+		if (Input.GetKeyUp (KeyCode.Alpha4))
+			UI(KeyCode.Alpha4);
+		//cast skill 5
+		if (Input.GetKeyUp (KeyCode.Alpha5))
+			UI(KeyCode.Alpha5);
 	}
 
 	void OnCollision(Collider collision){
@@ -72,5 +86,14 @@ public class TrainerInputHandler : MonoBehaviour {
 //		switch (collision.tag) {
 //			SharedConstants.
 //		}
+	}
+
+	//make sure trainer always faces camera
+	private void faceCamera(){
+		Vector3 facingAngle = camera.transform.eulerAngles;
+		Vector3 facePos = camera.transform.position;
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x, facingAngle.y, transform.eulerAngles.z);
+		camera.transform.eulerAngles = new Vector3(facingAngle.x, facingAngle.y, facingAngle.z); 
+		camera.transform.position = new Vector3(facePos.x, facePos.y, facePos.z); 
 	}
 }
