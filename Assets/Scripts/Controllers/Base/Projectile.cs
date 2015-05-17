b3eb6a125ab17095ffc6bf6620bef07f091f0e46
Michael <phanmicheal@gmail.com>
2015-05-17 16:21:51 -0400

    added projectile class

---
+++ Assets/Scripts/Controllers/Base/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : Destructible {
+	public int speed;
+	public Vector3 force;
+	public delegate void CollisionCallback(GameObject __obj);
+	public event CollisionCallback collided;
+	// Use this for initialization
+	void Awake () {
+		//do throw here
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+//	throwItem(force)
+//		overrideDestroy()
+			
+	public virtual void OnColliderHit(Collider __collision){
+		collided (__collision.gameObject);
+		DoDeath ();
+	}
+
+	public override void DoDeath(){
+		//death animations can go here too
+		GameObject.Destroy(this);
+	}
+}
