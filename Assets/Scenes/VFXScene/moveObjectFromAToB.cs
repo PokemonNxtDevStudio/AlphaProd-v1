e56f2195d090125f425b0dd64426d53902071a0e
abraham sanchez <abraham_gto@hotmail.com>
2015-05-25 20:06:12 -0500

    Lighting_v2

---
+++ Assets/Scenes/VFXScene/moveObjectFromAToB.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveObjectFromAToB : MonoBehaviour 
+{
+    public Transform[] Points;
+    public Transform PointB;
+    private Transform mytransform;
+    public float Speed = 10;
+    Transform next;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mytransform = gameObject.transform;
+        next = Points[1];
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+        gameObject.transform.position = Vector3.MoveTowards(mytransform.position, next.position, Speed);
+        if (mytransform.position == Points[1].position)
+        {
+            next = Points[2];
+        }
+        if (mytransform.position == Points[2].position)
+        {
+            next = Points[3];
+        }
+        if (mytransform.position == Points[3].position)
+        {
+            next = Points[0];
+        }
+        if (mytransform.position == Points[0].position)
+        {
+            next = Points[1];
+        }
+	
+	}
+}
