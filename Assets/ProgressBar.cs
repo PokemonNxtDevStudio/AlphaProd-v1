64084672183bc2b98d7af73f1fd787f1e68eb41e
Reuben <reubenp79@gmail.com>
2015-06-04 21:01:38 -0400

    Progress Bar

---
+++ Assets/ProgressBar.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ProgressBar : MonoBehaviour {
+
+
+
+    public int maxValue;
+
+    public float targetValue;
+    public float currentValue;
+    public int minValue;
+    private Image progressBar;
+    public bool enableSmoothing;
+    public float smoothingFactor= 9;
+	// Use this for initialization
+	
+    void Start () {
+        progressBar = GetComponent<Image>();
+	}
+
+    void setMaxValue(float maxValue)
+    {
+        maxValue = maxValue;
+    }
+	// Update is called once per frame
+	void Update () {
+
+        if (enableSmoothing)
+        {
+            if (!Mathf.Approximately(targetValue,currentValue))
+            {
+                currentValue = Mathf.Lerp(currentValue, targetValue, Time.deltaTime * smoothingFactor);
+                progressBar.fillAmount = currentValue / maxValue;
+            }
+            }
+        else
+            currentValue = targetValue;
+	}
+}
