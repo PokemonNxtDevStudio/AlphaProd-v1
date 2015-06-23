f425c16ca33d6d4df962addb71bc7d5c4d992e7a
abraham sanchez <abraham_gto@hotmail.com>
2015-06-23 18:42:18 -0500

    scratch v1

---
+++ Assets/Scenes/VFXScene/VFX/PlayVFX.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayVFX : MonoBehaviour 
+{
+    public ParticleSystem FVXToPlay;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+        if(Input.GetButtonDown("Fire1")/*GetKeyDown(/*KeyCode.Alpha0)*/)
+        {
+            if(FVXToPlay != null)
+            {
+                FVXToPlay.Play();
+                //Debug.Log("Playing " + FVXToPlay.name);
+            }
+        }
+	}
+}
