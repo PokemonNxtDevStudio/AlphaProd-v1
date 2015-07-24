bbed1de79aac7c08b5a0669e0c04275114c3a6e0
Reuben <reubenp79@gmail.com>
2015-07-23 22:24:00 -0400

    Temp Commit

---
+++ Assets/Scripts/UI/Framework/UIWindow.cs
@@ -0,0 +1,198 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+[RequireComponent(typeof(Canvas))]
+[RequireComponent(typeof(CanvasGroup))]
+
+enum WindowType
+{
+    BottomPanel,
+    Inventory,
+    Menu,
+    PokeParty
+}
+
+enum FadeType
+{
+    None,
+    FadeIn,
+    FadeOut
+}
+public class UIWindow :MonoBehaviour
+   {
+
+         public delegate void OnShowBegin();
+         public delegate void OnShowComplete();
+         public delegate void OnHideBegin();
+         public delegate void OnHideComplete();
+
+
+       /// <summary>
+       /// Each UIWindow is a group or subset of UI elements
+       /// </summary>
+ 
+       private static Dictionary<int,UIWindow> mWindows = new Dictionary<int,UIWindow> ();
+
+       public WindowType windowType = WindowType.UndefinedUI;
+       public Transform contentHolder;
+
+       int windowID { get { return (int)windowType; } }
+
+       public  bool isFocused;
+
+       public static int focusedWindow;
+        
+       public bool fadeEnabled = true;
+       public bool startHidden = true;
+       public bool hideOnLostFocus = false;
+       private CanvasGroup canvasGroup;
+       private Canvas canvas;
+       private int sortOrder;
+        
+       const int FrontOrder = 99;
+       const int BackOrder = -99;
+
+       bool isShowing;
+       public bool IsVisible
+       {
+          get { return ( this.gameObject.activeInHierarchy && this.canvasGroup.alpha!=1);}
+       }
+
+       void Awake()
+       {
+           if (mWindows.ContainsKey(this.windowID))
+           {
+               mWindows[this.windowID] = this;
+           }
+           else
+           {
+               mWindows.Add(this.windowID,this);
+           }
+       }
+       void Start()
+       {
+
+           if(canvasGroup==null)
+           canvasGroup = GetComponent<CanvasGroup>();
+           if(canvas == null)
+           canvas = GetComponent<Canvas>();
+           sortOrder = canvas.sortingLayerID;
+
+           HandleStartingState();
+       }
+
+       void HandleStartingState()
+       {
+
+           if (startHidden)
+           {
+               if (canvasGroup != null)
+                   this.canvasGroup.alpha = 0f;
+               contentHolder.gameObject.SetActive(false);
+               this.isShowing = false;
+           }
+           else
+           {
+               if (canvasGroup != null)
+                   this.canvasGroup.alpha = 1f;
+               contentHolder.gameObject.SetActive(true);
+               this.isShowing = true;
+
+           }
+
+       }
+
+       public static UIWindow GetWindow(WindowType windowType)
+       {
+           int id = (int)windowType;
+           if (mWindows.ContainsKey(id))
+               return mWindows[id];
+           return null;
+       }
+
+       public static UIWindow GetWindow(int id)
+       {
+           UIWindow window;
+
+           if (mWindows.TryGetValue(id, out window))
+               return window;
+
+           return window;
+       }
+
+       public static void FocusWindow(int id)
+       {
+           if (focusedWindow ==id)
+               return;
+         //  if(GetWindow(id)!=null)
+               //TODO
+       }
+        public static void FocusWindow(WindowType windowType)
+       {
+           FocusWindow((int)windowType);
+       }
+        public void Focus()
+        {
+
+            if (isFocused)
+                return;
+            this.isFocused = true;
+            this.canvas.sortingLayerID = FrontOrder;
+        }
+
+
+       
+        public void Show(bool instant = false,bool bringToFront = false)
+        {
+            if(!this.enabled || !gameObject.activeSelf)
+            {
+                this.gameObject.SetActive(true);
+            }
+            if(instant || !fadeEnabled )
+            {
+                this.canvasGroup.alpha = 1;
+                this.contentHolder.gameObject.SetActive(true);
+            }
+            else
+            {
+                this.contentHolder.gameObject.SetActive(true)// = true;
+                //TODO FADE IN
+            }
+            this.isShowing = true;
+        }
+
+        private FadeType animationCurrentMethod = FadeType.None;
+        private UICoroutine mFadeCoroutine;
+        
+
+    public void FadeIn(float duration)
+        {
+           
+        }
+
+    public class UICoroutine : IEnumerator
+    {
+        private bool stop;
+
+        IEnumerator enumerator;
+        MonoBehaviour behaviour;
+
+        public readonly Coroutine coroutine;
+
+        public UICoroutine(MonoBehaviour behaviour, IEnumerator enumerator)
+        {
+            this.behaviour = behaviour;
+            this.enumerator = enumerator;
+            this.coroutine = this.behaviour.StartCoroutine(this);
+        }
+
+        public object Current { get { return enumerator.Current; } }
+        public bool MoveNext() { return !stop && enumerator.MoveNext(); }
+        public void Reset() { enumerator.Reset(); }
+        public void Stop() { stop = true; }
+    }
+   }
+
